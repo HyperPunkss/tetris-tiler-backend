f91edf457af3a55a3dbe89fc8ed8f7e75f76f4a7
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-05-06 11:08:47 +0300

    Components for B1 Task

---
+++ src/main/java/com/hyperpunks/tetristilerbackend/web/controllers/B1TaskController.java
@@ -0,0 +1,25 @@
+package com.hyperpunks.tetristilerbackend.web.controllers;
+
+import com.hyperpunks.tetristilerbackend.library.B1Task;
+import com.hyperpunks.tetristilerbackend.web.services.B1TaskService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/a1-task")
+public class B1TaskController {
+
+    private final B1TaskService b1TaskService;
+
+    public B1TaskController(B1TaskService b1TaskService){
+        this.b1TaskService=b1TaskService;
+    }
+
+    @GetMapping("min-canvas-for-all-shapes")
+    public ResponseEntity<Object> minCanvas(){
+        return b1TaskService.fitAllShapes();
+    }
+}
---
+++ src/main/java/com/hyperpunks/tetristilerbackend/web/services/B1TaskService.java
@@ -0,0 +1,13 @@
+package com.hyperpunks.tetristilerbackend.web.services;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class B1TaskService {
+
+    public ResponseEntity<Object> fitAllShapes(){
+        return null;
+    }
+}
