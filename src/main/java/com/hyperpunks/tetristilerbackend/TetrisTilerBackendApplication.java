17e355f387b7d16501f99ec078719c271b9502a1
Maciej Ratkiewicz <matsaki95@tutanota.com>
2023-05-06 09:04:50 +0300

    Initial commit

---
+++ src/main/java/com/hyperpunks/tetristilerbackend/TetrisTilerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hyperpunks.tetristilerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetrisTilerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TetrisTilerBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyperpunks/tetristilerbackend/TetrisTilerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperpunks.tetristilerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetrisTilerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
