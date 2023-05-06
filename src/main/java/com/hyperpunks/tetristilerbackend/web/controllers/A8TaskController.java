23e427d5fbf63244ad0365d0243225fc12a6cfc8
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-05-07 02:16:29 +0300

    Fixed CORS problem with A8TaskController

--- src/main/java/com/hyperpunks/tetristilerbackend/web/controllers/A8TaskController.java
+++ src/main/java/com/hyperpunks/tetristilerbackend/web/controllers/A8TaskController.java
@@ -2,12 +2,14 @@ package com.hyperpunks.tetristilerbackend.web.controllers;
 
 import com.hyperpunks.tetristilerbackend.web.services.A8TaskService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/v1/a8-task")
+@CrossOrigin(origins = "*")
 public class A8TaskController {
 
     private final A8TaskService a8TaskService;
