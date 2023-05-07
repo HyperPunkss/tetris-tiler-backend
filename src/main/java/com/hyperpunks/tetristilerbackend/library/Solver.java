51f71af0a193440804b8ff8cd74d5181999cd289
Maciej Ratkiewicz <matsaki95@tutanota.com>
2023-05-07 10:30:25 +0300

    Fixed passing by pointer and not by value _because this stuff tends to not be clear in java_

--- src/main/java/com/hyperpunks/tetristilerbackend/library/Solver.java
+++ src/main/java/com/hyperpunks/tetristilerbackend/library/Solver.java
@@ -6,19 +6,34 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Solver {
+    static void permute(java.util.List<Integer> arr, int k){
+        for(int i = k; i < arr.size(); i++){
+            java.util.Collections.swap(arr, i, k);
+            permute(arr, k+1);
+            java.util.Collections.swap(arr, k, i);
+        }
+        if (k == arr.size() -1){
+            System.out.println(java.util.Arrays.toString(arr.toArray()));
+        }
+    }
 
     public static Set<String> findAllSolutions(Grid startingGrid, List<Shape> shapes, boolean canRotate, boolean canFlip) {
         Set<String> solutions = new TreeSet<>();
         if (startingGrid.countUnfilled() % 5 != 0) {
             return solutions;
         }
-        List<List<Shape>> allShapesAndVariants = shapes.stream().map(shape -> shape.generateAllVariants(canRotate, canFlip)).toList();
+
+        List<List<Shape>> allShapesAndVariants = new ArrayList<>();
+        for (Shape shape : shapes) {
+            allShapesAndVariants.add(shape.generateAllVariants(canRotate, canFlip));
+        }
+
         List<Grid> gridStates = new ArrayList<>();
         gridStates.add(startingGrid);
         List<Grid> nextGridStates = new ArrayList<>();
         for (List<Shape> shape : allShapesAndVariants) {
             for (Shape shapeVariant : shape) {
-                for (int i =0; i < gridStates.size(); i++) {
+                for (int i = 0; i < gridStates.size(); i++) {
                     Grid stateGrid = gridStates.get(i);
                     Grid nextGrid = stateGrid.clone();
                     List<int[]> allPlacements;
@@ -50,7 +65,8 @@ public class Solver {
                     }
                 }
             }
-            gridStates = nextGridStates;
+            gridStates.clear();
+            gridStates.addAll(nextGridStates);
         }
         return solutions;
     }
